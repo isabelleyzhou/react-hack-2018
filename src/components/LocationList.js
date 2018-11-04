29688fae5cf1116aebbb16345e0962c2f25602c7
Richard <richardliuwa@gmail.com>
2018-11-04 11:19:01 -0800

    partially complete

--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -28,14 +28,14 @@ class LocationList extends Component {
     if (location){
       let self = this
       location.getCurrentPosition((position) => {
-        let x1 = 37.77;
-        let dx1 = Math.random()*0.02;
-        let y1 = -122.25;
-        let dy1 = Math.random()*0.02;
-        let x2 = 37.77;
-        let dx2 = Math.random()*0.02;
-        let y2 = -122.25;
-        let dy2 = Math.random()*0.02;
+        let x1 = 37.87;
+        let dx1 = Math.random()*0.005;
+        let y1 = -122.252;
+        let dy1 = Math.random()*0.005;
+        let x2 = 37.87;
+        let dx2 = Math.random()*0.005;
+        let y2 = -122.252;
+        let dy2 = Math.random()*0.005;
         lt = position.coords.latitude;
         ln = position.coords.longitude;
         self.setState ({
@@ -54,6 +54,25 @@ class LocationList extends Component {
         });
       }
       )}
+
+      let count = 1;
+      let x = this.state.latitude;
+      let y = this.state.longitude;
+      for (let key in this.state.other_friends) {
+        let val = this.state.other_friends[key];
+        x += val[0];
+        y += val[1];
+        count += 1;
+      }
+      x = x / count;
+      y = y / count;
+      if (!isNaN(x) && !isNaN(y)) {
+        let locations = this.state.locations;
+        console.log(locations);
+      }
+
+
+
     this.search = this.search.bind(this);
     this.findMiddleRestaurants= this.findMiddleRestaurants.bind(this); 
   }
@@ -79,59 +98,49 @@ class LocationList extends Component {
   }
 
   findMiddleRestaurants() {
-    let count = 1;
-    let x = this.state.latitude;
-    let y = this.state.longitude;
-    for (let key in this.state.other_friends) {
-      let val = this.state.other_friends[key];
-      x += val[0];
-      y += val[1];
-      count += 1;
-    }
-    x = x / count;
-    y = y / count;
-    if (!isNaN(x) && !isNaN(y)) {
-      let locations = this.state.locations;
-      console.log(locations);
-    }
+
   }
 
 
-  async componentDidMount() {
-    const RapidAPI = await require('rapidapi-connect');
-    const rapid = await new RapidAPI("default-application_5bde08cde4b09efa5fbce273", "a3092fd1-0bb6-447e-bc02-287407fe8392");
+  async componentWillMount() {
+    if (this.state.loaded1) {
+      const RapidAPI = await require('rapidapi-connect');
+      const rapid = await new RapidAPI("default-application_5bde08cde4b09efa5fbce273", "a3092fd1-0bb6-447e-bc02-287407fe8392");
 
-    rapid.call('YelpAPI', 'getBusinesses', {
-      accessToken: 'f8NSZ7Sz4Ik5GGmMJ5MesAO15azeGlsGM-yT6wlU-scRpc1Q3whzwjal0T34QC_oXvXjwRoXu0sX4OIboGcjBm4JrHiWr8RKQA7YYUe-3n7E6eDWESxzKei3EgjeW3Yx',
-      coordinate: '37.87097, -122.2559',
-      limit: '3',
-      sortBy: 'distance'
+      rapid.call('YelpAPI', 'getBusinesses', {
+        accessToken: 'f8NSZ7Sz4Ik5GGmMJ5MesAO15azeGlsGM-yT6wlU-scRpc1Q3whzwjal0T34QC_oXvXjwRoXu0sX4OIboGcjBm4JrHiWr8RKQA7YYUe-3n7E6eDWESxzKei3EgjeW3Yx',
+        coordinate: this.state.target,
+        limit: '3',
+        sortBy: 'distance'
+  
+      }).on('success', (payload)=>{
+        let names;
+        let images;
+        let numbers;
+        let distances;
+        let ratings;
+        let phones;
+        let prices;
+        let coordinatesList;
+        for (let i = 0; i < payload["businesses"].length; i += 1) {
+          names = payload["businesses"][i]["name"];
+          images = payload["businesses"][i]["image_url"];
+          numbers = i+1;
+          ratings = payload["businesses"][i]["rating"];
+          phones = payload["businesses"][i]["phone"];
+          prices = payload["businesses"][i]["price"];
+          distances = (payload["businesses"][i]["distance"]/225).toFixed(2);
+          coordinatesList = payload["businesses"][i]["coordinates"];
+          this.setState({
+            loaded2: true, locations: this.state.locations.concat(<Location image={images} locationName={names} number={numbers} milesAway={distances} phone={phones} rating={ratings} price={prices} coordinates={coordinatesList}/>)
+          })
+        }
+      }).on('error', (payload)=>{
+        console.warn('error');
+      });
+    } 
+    
 
-    }).on('success', (payload)=>{
-      let names;
-      let images;
-      let numbers;
-      let distances;
-      let ratings;
-      let phones;
-      let prices;
-      let coordinatesList;
-      for (let i = 0; i < payload["businesses"].length; i += 1) {
-        names = payload["businesses"][i]["name"];
-        images = payload["businesses"][i]["image_url"];
-        numbers = i+1;
-        ratings = payload["businesses"][i]["rating"];
-        phones = payload["businesses"][i]["phone"];
-        prices = payload["businesses"][i]["price"];
-        distances = (payload["businesses"][i]["distance"]/225).toFixed(2);
-        coordinatesList = payload["businesses"][i]["coordinates"];
-        this.setState({
-          loaded2: true, locations: this.state.locations.concat(<Location image={images} locationName={names} number={numbers} milesAway={distances} phone={phones} rating={ratings} price={prices} coordinates={coordinatesList}/>)
-        })
-      }
-    }).on('error', (payload)=>{
-      console.warn('error');
-    });
   }
 
   renderMarkers(map, maps) {
