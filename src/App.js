5462ddf8ee78a693924122f25584739b57a0d0d8
Andrew Lieu <Andrewlieu@berkeley.edu>
2018-11-03 18:33:16 -0700

    dataPage completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import firebase, { config, login, logout } from './firebase-config';
+import DatePage from './components/datePage';
 
 function displayContent(user) {
   const firstName = user.displayName.split(' ')[0];
@@ -12,20 +13,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p id="name">
-            {login(displayContent)}
-          </p>
-          <a
-            onClick={logout}
-            className="App-link"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Log Out
-          </a>
-        </header>
+        <DatePage></DatePage>
       </div>
     );
   }
