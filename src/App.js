5638f7653d5b4d067500a5134bc31e55dbb2ff83
Richard <richardliuwa@gmail.com>
2018-11-03 17:21:41 -0700

    added authentication and firebase integration

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase, { config, login, logout } from './firebase-config';
+
+function displayContent(user) {
+  const firstName = user.displayName.split(' ')[0];
+  document.getElementById("name").innerHTML = firstName;
+}
 
 class App extends Component {
   render() {
@@ -8,16 +14,16 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+          <p id="name">
+            {login(displayContent)}
           </p>
           <a
+            onClick={logout}
             className="App-link"
-            href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Log Out
           </a>
         </header>
       </div>
---
+++ src/firebase-config.js
@@ -0,0 +1,30 @@
+import firebase from 'firebase';
+
+export const config = {
+    apiKey: "AIzaSyD5RjSHkTzUhDfg5rvDngEGf8LGDF0hviY",
+    authDomain: "wheretwomeet.firebaseapp.com",
+    databaseURL: "https://wheretwomeet.firebaseio.com",
+    projectId: "wheretwomeet",
+    storageBucket: "wheretwomeet.appspot.com",
+    messagingSenderId: "37099806359"
+};
+
+firebase.initializeApp(config);
+export default firebase;
+
+export function login(userFunction) {
+    function newLoginHappened(user) {
+      if (user) {
+        userFunction(user);
+      } else {
+        var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithRedirect(provider);
+      }
+    }
+    firebase.auth().onAuthStateChanged(newLoginHappened);
+}
+
+export function logout(user, userFunction=null) {
+  firebase.auth().signOut();
+  userFunction(user);
+}
